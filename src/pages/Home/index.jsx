f880dcc918bb876441b11708f645d7e2d6424a70
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-12-07 14:10:01 -0300

    Home (1 - searchBox)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,30 @@
+import { PageContainer } from "../../components";
+import { PageArea, SearchArea } from "./styled";
 function Home() {
-    return (
-        <div>
-            <h2>Página inicial</h2>
-        </div>
-    );
+  return (
+    <>
+      <SearchArea>
+        <PageContainer>
+          <div className="searchBox">
+            <form method="GET" action="/ads">
+              <input type="text" name="q" placeholder="O que você procura?" />
+              <select name="state">
+                <option value="">Escolha uma opção</option>
+              </select>
+              <button>Pesquisar</button>
+            </form>
+          </div>
+          <div className="categoryList"></div>
+        </PageContainer>
+      </SearchArea>
+
+      <PageContainer>
+        <PageArea>
+          <h2>Página inicial</h2>
+        </PageArea>
+      </PageContainer>
+    </>
+  );
 }
 
 export default Home;
