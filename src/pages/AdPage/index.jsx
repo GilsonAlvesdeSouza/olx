7e44e520289c90bedf8170f4accf5675fdd1a829
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-12-08 13:47:20 -0300

    AdPage (5 - Info Direita)

--- src/pages/AdPage/index.jsx
+++ src/pages/AdPage/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import 'react-slideshow-image/dist/styles.css';
-import {Slide} from "react-slideshow-image"
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
 import { OlxAPI, formater } from "../../helpers";
 import { PageArea } from "./styled";
 import { PageContainer } from "../../components";
@@ -23,12 +23,12 @@ function AdPage() {
     getInfo(id);
   }, [id]);
 
-  const handleSlideImages=()=>{
-      return info.images.map((item, key) =>(
-          <div key={`img-${key}`} className="each-slide">
-              <img src={item} alt="" />
-          </div>
-      ) );
+  const handleSlideImages = () => {
+    return info.images.map((item, key) => (
+      <div key={`img-${key}`} className="each-slide">
+        <img src={item} alt="" />
+      </div>
+    ));
   };
 
   return (
@@ -37,13 +37,9 @@ function AdPage() {
         <div className="left-side">
           <div className="box">
             <div className="ad-image">
-                {loading && <Fake height="300" />}
-                {info.images && 
-                <Slide>
-                    {handleSlideImages()}
-                </Slide>
-                }
-                </div>
+              {loading && <Fake height="300" />}
+              {info.images && <Slide>{handleSlideImages()}</Slide>}
+            </div>
             <div className="ad-info">
               <div className="ad-name">
                 {loading && <Fake />}
@@ -62,10 +58,33 @@ function AdPage() {
           </div>
         </div>
         <div className="right-side">
-          <div className="box box--padding">{loading && <Fake />}</div>
           <div className="box box--padding">
-            {loading && <Fake height="50" />}
+            {loading && <Fake />}
+            {info.priceNegotiable && "Preço Negociável"}
+            {!info.priceNegotiable && info.price && (
+              <div className="price">
+                Preço: <span>{format.formatCurrency(info.price)}</span>
+              </div>
+            )}
           </div>
+          {loading && <Fake height="50" />}
+          {info.userInfo && (
+            <>
+              <a
+                href={`mailto:${info.userInfo.email}`}
+                target="_blank"
+                rel="noreferrer"
+                className="contactSellerLink"
+              >
+                Fale com o Anunciante
+              </a>
+              <div className="created-by box box--padding">
+                <strong>{info.userInfo.name}</strong>
+                <small> {info.userInfo.email}</small>
+                <small>Estado: {info.stateName}</small>
+              </div>
+            </>
+          )}
         </div>
       </PageArea>
     </PageContainer>
