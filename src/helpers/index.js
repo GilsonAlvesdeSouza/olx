b288b76f199ad9563e2989bf836d178cd8d8c8b4
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-12-14 12:13:38 -0300

    Ads (2 - leftSide)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,6 @@
 import { isLogged, doLogin, doLogout } from "./AuthHandler";
 import OlxAPI from "./OlxAPI";
 import formater from "./Formater";
+import { useQueryString } from "./URLHelpers";
 
-export { isLogged, doLogin, OlxAPI, doLogout, formater };
+export { isLogged, doLogin, OlxAPI, doLogout, formater, useQueryString };
--- src/pages/Ads/index.jsx
+++ src/pages/Ads/index.jsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { PageContainer, AdItem } from "../../components";
-import { OlxAPI } from "../../helpers";
+import { OlxAPI, useQueryString } from "../../helpers";
 import { PageArea } from "./styled";
 
 function Ads() {
   const [stateList, setStateList] = useState([]);
   const [categories, setCategories] = useState([]);
   const [adList, setAdList] = useState([]);
+
+  const query = useQueryString();
+  const initialQuery = (value) => {
+    return query.get(value) != null ? query.get(value) : "";
+  };
+  const [q, setQ] = useState(initialQuery("q"));
+  const [cats, setCats] = useState(initialQuery("cats"));
+  const [state, setState] = useState(initialQuery("state"));
 
   useEffect(() => {
     let api = OlxAPI();
@@ -47,7 +55,7 @@ function Ads() {
     return categories.map((item, key) => (
       <li
         key={`categories-${key}`}
-        className="categoryItem"
+        className={cats === item.slug ? "categoryItem active" : "categoryItem"}
       >
         <img src={item.img} alt="" />
         <span>{item.name}</span>
@@ -62,27 +70,28 @@ function Ads() {
   };
 
   return (
-   <PageContainer>
-       <PageArea>
-           <div className="leftSide">
-              <form method="GET">
-                <input type="text" name="q"/>
-                <div className="filterName">Estado: </div>
-                <select name="state">
-                  <option value="">Escolha um estado</option>
-                  {handleStateOption()}
-                </select>
-                <div className="filterName">Categoria: </div>
-                <ul>
-                  {handleCategories()}
-                </ul>
-              </form>
-           </div>
-           <div className="right Side">
-                ...
-           </div>
-       </PageArea>
-   </PageContainer>
+    <PageContainer>
+      <PageArea>
+        <div className="leftSide">
+          <form method="GET">
+            <input
+              type="text"
+              name="q"
+              placeholder="O que você procura?"
+              value={q}
+            />
+            <div className="filterName">Estado: </div>
+            <select name="state" value={state}>
+              <option value="">Escolha um estado</option>
+              {handleStateOption()}
+            </select>
+            <div className="filterName">Categoria: </div>
+            <ul>{handleCategories()}</ul>
+          </form>
+        </div>
+        <div className="right Side">...</div>
+      </PageArea>
+    </PageContainer>
   );
 }
 
